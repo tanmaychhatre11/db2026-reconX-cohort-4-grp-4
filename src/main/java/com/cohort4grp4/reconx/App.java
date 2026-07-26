4d6fedb3933e6acf7df256d14a624a264d59b18b
Tanmay Chhatre <158827760+tanmaychhatre11@users.noreply.github.com>
2026-07-26 17:36:54 +0530

    Scaffold Maven project (#4)
    * scaffold maven project structure
    
    * feat: add ci.yml
    
    * fix: lint job error
    
    * fix: all job errors
    
    * fix: fail on warnings flipped to false

---
+++ src/main/java/com/cohort4grp4/reconx/App.java
@@ -0,0 +1,13 @@
+package com.cohort4grp4.reconx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

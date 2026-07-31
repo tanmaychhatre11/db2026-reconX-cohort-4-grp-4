16b382d13f4408507158308f7ec9db76f21df652
Tanmay Chhatre <tanchhatre8114@gmail.com>
2026-07-31 12:15:25 +0530

    fix: adv79 correct migration test file

--- backend/src/test/java/com/dbtraining/reconx/integration/LiquibaseMigrationsIT.java
+++ backend/src/test/java/com/dbtraining/reconx/integration/LiquibaseMigrationsIT.java
@@ -1,4 +1,4 @@
-package com.dbtraining.reconx;
+package com.dbtraining.reconx.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("uat")
 class LiquibaseMigrationsIT {
 
     @Container
